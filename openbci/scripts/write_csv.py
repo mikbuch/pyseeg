87998a0fc4d45edbe89f07e11cc3291d907babc2
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-06-12 00:00:52 +0200

    major commmit, most things working, some sample data

---
+++ openbci/scripts/write_csv.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('..')
+import open_bci_v3 as bci
+import csv
+import datetime
+
+output_dir = '../../data/'
+
+# code the time to name file or variable
+csv_filename = output_dir + \
+    datetime.datetime.now().strftime("%Y%m%d%H%M") + \
+    '.csv'
+
+
+def printData(sample):
+    print "----------------"
+#     print("%f" %(sample.id))
+#     print sample.channel_data
+    print sample.channel_data[0]
+#     print sample.aux_data
+    print "----------------"
+    with open(csv_filename, 'at') as f:
+        save = csv.writer(f)
+        save.writerow(sample.channel_data)
+        # save.writerow([sample.id] + sample.channel_data)
+
+if __name__ == '__main__':
+    port = '/dev/ttyUSB0'
+    baud = 115200
+    board = bci.OpenBCIBoard(port=port)
+    board.start_streaming(printData)
