c2103abe13fc2a080a2fd5c7229123bc28a7044d
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-06-12 00:00:52 +0200

    major commmit, most things working, some sample data

--- examples/flt_test.py
+++ examples/flt_test.py
@@ -15,18 +15,15 @@ import modules.filterlib as flt
 fs = 250.0
 
 # bandpass values
-lowcut = 1.0
-highcut = 50.0
-
-# bandstop values
-lowstop = 49.0
-highstop = 51.0
+lowcut = 7.0
+highcut = 13.0
 
 # file with eeg data location
-eeg_file = 'data/201506111703.csv'
+eeg_file = 'data/flt_no_flt.txt'
 
 # read the eeg file to array
-data = read(eeg_file, delimiter=',', header=1)
+data = read(eeg_file, delimiter='\t', header=1)
+data_pre_flted = [float(i[1]) for i in data]
 data = [float(i[0]) for i in data]
 
 # raw data from specified range
@@ -35,26 +32,19 @@ data = [float(i[0]) for i in data]
 # required for futher plotting appropriate x axis (samples)
 time_axis = range(len(data))
 
-# plot raw signal
-plt.plot(time_axis, data)
-plt.show()
-
-# filter data (using butt bandstop filter)
-flted = flt.butter_bandstop_filter(data, lowstop, highstop, fs, order=2)
 # filter data (using butt bandpass filter)
-flted = flt.butter_bandpass_filter(flted, lowcut, highcut, fs, order=2)
+flted = flt.butter_bandpass_filter(data, lowcut, highcut, fs, order=2)
 
 # plot time domain of the signal
 plt.plot(time_axis, flted)
+plt.plot(time_axis, data_pre_flted, '-r')
 plt.show()
 
 # plot frequency domain of the signal
-freq = abs(2 * np.fft.fft(flted))/len(data)
+freq = abs(2 * np.fft.fft(flted))/512
 freq = freq[:fs]
 plt.plot(freq)
-plt.show()
-
-# plot frequency domain of the signal
-freq_all = abs(2 * np.fft.fft(data))/len(data)
-plt.plot(freq_all)
+freq_pre_flted = abs(2 * np.fft.fft(data_pre_flted))/512
+freq_pre_flted = freq_pre_flted[:fs]
+plt.plot(freq_pre_flted, '-r')
 plt.show()
