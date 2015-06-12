a0f7ae0e35a4c107695bdb5eba594ea305787d32
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-06-12 17:54:53 +0200

    main commit. filtering and plotiing working

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ import matplotlib.pyplot as plt
 from modules.read_csv import read
 import modules.filterlib as flt
 
+############################################
+#                                          #
+#           VARIABLES INIT                 #
+#                                          #
+############################################
 # sampling frequency (how many samples per second)
 fs = 250.0
 
@@ -25,36 +30,99 @@ highstop = 51.0
 # file with eeg data location
 eeg_file = 'data/201506111703.csv'
 
+# seconds analysed
+sec_beg = 15
+sec_end = 20
+sec_rng = sec_end-sec_beg
+
+# lower and higher range values
+rng = [sec_beg*int(fs), sec_end*int(fs)]
+
+############################################
+#                                          #
+#          GET DATA FROM FILE              #
+#                                          #
+############################################
 # read the eeg file to array
 data = read(eeg_file, delimiter=',', header=1)
 data = [float(i[0]) for i in data]
 
-# raw data from specified range
-# rng_data = data[sec_begin*fs:sec_end*fs]
+data_rng = data[rng[0]:rng[1]]
 
 # required for futher plotting appropriate x axis (samples)
-time_axis = range(len(data))
+x_time = range(len(data))
+x_time = [i/fs for i in x_time]
+x_time_rng = x_time[rng[0]:rng[1]]
 
-# plot raw signal
-plt.plot(time_axis, data)
-plt.show()
+############################################
+#                                          #
+#               FILTERING                  #
+#                                          #
+############################################
+# filter data using butt bandstop 49-51 Hz filter (50Hz)
+flted_50_stop = flt.butter_bandstop_filter(
+    data, lowstop, highstop, fs, order=2
+    )
+# bandpass filter only
+flted_1_50_pass_only = flt.butter_bandpass_filter(
+    data, lowcut, highcut, fs, order=2
+    )
+# filter prefiltered 50_stop data
+flted_1_50_pass = flt.butter_bandpass_filter(
+    flted_50_stop, lowcut, highcut, fs, order=2
+    )
 
-# filter data (using butt bandstop filter)
-flted = flt.butter_bandstop_filter(data, lowstop, highstop, fs, order=2)
-# filter data (using butt bandpass filter)
-flted = flt.butter_bandpass_filter(flted, lowcut, highcut, fs, order=2)
+############################################
+#                                          #
+#            FOURIER TRANSFORM             #
+#                                          #
+############################################
+# choose filtered data type
+flted = flted_1_50_pass
+# get from the filtered data range (in seconds)
+flted_rng = flted[rng[0]:rng[1]]
 
-# plot time domain of the signal
-plt.plot(time_axis, flted)
-plt.show()
+# fourier transform, abs
+freq = abs(2 * np.fft.fft(flted_rng))/sec_rng*fs
+# cut spectrum half
+freq = freq[:len(freq)/2]
 
-# plot frequency domain of the signal
-freq = abs(2 * np.fft.fft(flted))/len(data)
-freq = freq[:fs]
-plt.plot(freq)
-plt.show()
+# create time frequency axis
+x_freq = [i/float(5) for i in range(len(freq))]
 
-# plot frequency domain of the signal
-freq_all = abs(2 * np.fft.fft(data))/len(data)
-plt.plot(freq_all)
+# fourier transform for complete data
+freq_all = abs(2 * np.fft.fft(flted))/len(flted)
+freq_all = freq_all[:len(freq_all)/2]
+
+# create time frequency axis (for complete data)
+x_freq_all = [i/float(5) for i in range(len(freq_all))]
+
+############################################
+#                                          #
+#             PLOTTING DATA                #
+#                                          #
+############################################
+# raw signal time domain plot
+plt.subplot(2, 3, 1)
+plt.plot(x_time, data)
+
+# filtered signal time domain plot
+plt.subplot(2, 3, 2)
+plt.plot(x_time, flted)
+
+# frequency domain of the signal (power spectrum) plot
+plt.subplot(2, 3, 3)
+plt.plot(x_freq_all, freq_all)
+
+# raw signal time domain in range
+plt.subplot(2, 3, 4)
+plt.plot(x_time_rng, data_rng)
+
+# filtered time domain in range
+plt.subplot(2, 3, 5)
+plt.plot(x_time_rng, flted_rng)
+
+# frequency domain of the signal (power spectrum) in range
+plt.subplot(2, 3, 6)
+plt.plot(x_freq, freq)
 plt.show()
