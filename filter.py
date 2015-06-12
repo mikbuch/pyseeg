f28b6bec34c58ac9cd7a104a6c381e5d2bcfbc0d
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-06-12 17:54:53 +0200

    main commit. filtering and plotiing working

--- filter.py
+++ filter.py
@@ -8,14 +8,14 @@ prev_y = np.zeros((8, 5))
 prev_x2 = np.zeros((8, 5))
 prev_y2 = np.zeros((8, 5))
 
-flt_type = 0
+flt_type = 6
 
 
 def filterIIR(data, nrk):
-    b = 0.0
-    a = 0.0
-    b2 = 0.0
-    a2 = 0.0
+    # b = 0.0
+    # a = 0.0
+    # b2 = 0.0
+    # a2 = 0.0
 
     j = 5 - 1
     while j > 0:
@@ -36,8 +36,12 @@ def filterIIR(data, nrk):
             0.2001387256580675
             )
         a = np.array(
-            1, -2.355934631131582,
-            1.941257088655214, -0.7847063755334187, 0.1999076052968340)
+            1,
+            -2.355934631131582,
+            1.941257088655214,
+            -0.7847063755334187,
+            0.1999076052968340
+            )
 
     elif (flt_type == 2):  # 7-13Hz
         b = np.array(
@@ -48,7 +52,10 @@ def filterIIR(data, nrk):
             0.005129268366104263)
         a = np.array(
             1, -3.678895469764040,
-            5.179700413522124, -3.305801890016702, 0.8079495914209149)
+            5.179700413522124,
+            -3.305801890016702,
+            0.8079495914209149
+            )
 
     elif (flt_type == 3):  # 15-50Hz
         b = np.array(
@@ -103,14 +110,44 @@ def filterIIR(data, nrk):
             )
 
     elif (flt_type == 7):  # 60 Hz
-        b2 = np.array(0.9650809863447347, -0.2424683201757643,
-            1.945391494128786, -0.2424683201757643, 0.9650809863447347)
-        a2 = np.array(1, -0.2467782611297853,
-            1.944171784691352, -0.2381583792217435, 0.9313816821269039)
+        b2 = np.array(
+            0.9650809863447347,
+            -0.2424683201757643,
+            1.945391494128786,
+            -0.2424683201757643,
+            0.9650809863447347
+            )
+        a2 = np.array(
+            1,
+            -0.2467782611297853,
+            1.944171784691352,
+            -0.2381583792217435,
+            0.9313816821269039
+            )
 
     elif (flt_type == 8):  # none
         b2 = np.array(1, 1, 1, 1, 1)
         a2 = np.array(1, 1, 1, 1, 1)
 
-    filtered = filtruj(b2, a2, b, a, nrk);
-    return filtered;
+    filtered = filter_data(b2, a2, b, a, nrk)
+    return filtered
+
+
+def filter_data(b2, a2, b, a, nrk):
+    wynik = 0.0
+    for j in range(5):
+        wynik += b2[j] * prev_x[nrk, j]
+        if j > 0:
+            wynik -= a2[j] * prev_y[nrk, j]
+    prev_y[nrk, 0] = wynik
+    prev_x2[nrk, 0] = wynik
+    wynik = 0.0
+    for j in range(5):
+        wynik += b[j] * prev_x2[nrk, j]
+        if j > 0:
+            wynik -= a[j] * prev_y2[nrk, j]
+    prev_y2[nrk, 0] = wynik
+    return wynik
+
+x = -581.32344338
+filterIIR(x, 0)
