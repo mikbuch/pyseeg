f7a3875cb9b24f17e142366e52cf66b09e331465
root <root@acjmhost>
2017-10-11 23:41:00 +0200

    ssveplib real time working

--- pyseeg/modules/ssveplib.py
+++ pyseeg/modules/ssveplib.py
@@ -1,60 +1,63 @@
 import numpy as np
-from pyseeg.modules.fft import transform
+from pyseeg.modules.fft import _transform
 
 
 class SSVEPRealTime(object):
 
-    def __init__(self, fs, window_len, freq_ranges, window_kind='nooverpal'):
-        
+    def __init__(self, fs, window_len, freqs, window_kind='nooverpal'):
+        '''
+            fs (int or float) - sampling frequency
+            window_len (int)  - length of a window (numer of samples to
+                                analyze)
+            freqs (int or floa) - requency ranges, the classifier has to know
+                                  of which frequencies the choice has to be
+                                  made
+            window_kind (bool) - if two windows can contain the same samples
+        '''
+
         self.fs = fs
         self.window_len = window_len
         self.window_kind = window_kind
 
         self.data = np.zeros(self.window_len)
+        # For couning samples within window.
         self.cnt = 0
 
-        self.freq_ranges = freq_ranges
+        self.freqs = freqs
 
-        self.x_freq = [i/window_len/(float(fs)/2.)) for i in range(window_len)]
+        # We have to know which vector element to choose by frequency value,
+        # not by the index of the vector.
+        self.x_freq = np.array([i/(float(window_len)/(float(fs)/2.))
+                                for i in range(window_len)])
+
 
         self.decision = 0
-    
 
     def ssvep_detect(self, smp):
+        '''
+            smp (float) - single, filtered sample
+        '''
 
         self.data[self.cnt] = smp
         self.cnt += 1
 
         if self.cnt == self.window_len:
 
-            data_fft = transform(self.data)
+            data_fft = _transform(self.data)
 
+            # Mean frequency amplitude.
             mean_freqs = []
 
-            freq_idxs = []
-            for (low, hi) in freq_ranges:
-                low_idx = np.argmin(np.abs(x_freq - low))
-                hi_idx = np.argmin(np.abs(x_freq - hi))
+            for freq in self.freqs:
+                # Get the indices, values within +/-1 frequency value.
+                low_idx = np.argmin(np.abs(self.x_freq - freq)) - 1
+                hi_idx = np.argmin(np.abs(self.x_freq - freq)) + 1
 
-                freq_idxs.append([low_idx, hi_idx])
-            
-            for idxs in freq_idxs:
-                mean_freqs.append(data_ffs[idxs[0]:idxs[1]])
-            
+                mean_freqs.append(np.mean(data_fft[low_idx:hi_idx]))
+
             self.cnt = 0
 
+            print(mean_freqs)
             self.decision = np.argmax(mean_freqs)
 
         return self.decision
-
-    
-
-
-
-    # def zzz(self):
-
-        # print(self.data)
-
-
-# srt = slt.SSVEPRealTime(window_kind='nooverlap', window_len=500, fs=250)
-# srt.zzz()
