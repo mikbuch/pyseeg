152d2134921f4d57abe2f03c3293d486c29e71b2
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2018-01-08 13:01:51 +0100

    trigger only option

--- pyseeg/visualization/checkout_plot.py
+++ pyseeg/visualization/checkout_plot.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_data(path, sep=',', header=None):
+def plot_data(path, sep=',', header=None, trigger_only=False):
     data = pd.read_csv(path, sep=sep, header=header)
-    data.plot(subplots=True, title=path)
+    if not trigger_only:
+        data.plot(subplots=True, title=path)
+    else:
+        plt.plot(data[5])
+        plt.ylim(-0.1,1.1)
     plt.show()
